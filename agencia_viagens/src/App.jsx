c2c52cb70543c69084ead28e074b340a366fea6c
weslleycamposc2p1 <weslleycampos951@gmail.com>
2024-10-14 11:52:37 -0300

    agencia de viagens

---
+++ agencia_viagens/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './assets/Components/Header'
+import Home from './assets/Components/Home'
+import Rodape from './assets/Components/Rodape'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Home />
+      <Rodape />
+    </>
+  )
+}
+
+export default App
